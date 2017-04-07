f2f80fa0e6396bc73f312da77d9316ca6c3aa2a2
jschatz50 <jschatz50@hotmail.com>
2017-04-06 20:22:36 -0600

    Add files via upload

--- gsv_processing.py
+++ gsv_processing.py
@@ -24,16 +24,20 @@ import pandas as pd
 import scipy.misc
 from scipy.misc import imread
 import sys
+import time
 import urllib
 
 
 #----------------------#
 #-- define functions --#
 #----------------------#
-#### download GSV images for a set of locations, for all specified headings and pitches
-## @param locations:  dataframe with SID, LAT, and LON columns (SID = sensorID)
-## @param outpath:  path for downloaded images
 def get_GSV(locations, outpath):
+  '''download GSV images for a set of locations, for all specified headings and pitches
+
+    Args:
+      locations: pandas df with SID, LAT, and LON columns (SID = sensorID)
+      outpath: path for downloaded images
+  '''
    for row in range(len(locations)):
       lat = locations.iloc[row]['LAT']
       lon = locations.iloc[row]['LON']
@@ -46,47 +50,58 @@ def get_GSV(locations, outpath):
             filename = sid + '_' + str(heading) + '_' + str(pitch) + ".jpg"
             urllib.urlretrieve(temp_url, outpath + filename)
 
-#### calculate percent green vegetation from GSV image
-#### same rules as Li et al 2015:
-#### (1) product of green minus red and green minus blue is positive
-#### (2) green band is greater than the red band (in case both diffs are negative)
-## @param r: red band
-## @param g: green band
-## @param b: blue band
-def percent_veg(r, g, b):
+def percent_veg(r, g, b, pieces):
+  '''calculate percent green canopy from GSV image using same rules as Li et al 2015:
+    (1) product of green minus red and green minus blue is positive
+    (2) green band is greater than the red band (in case both diffs are negative)
+
+    Args:
+      r: red band
+      g: green band
+      b: blue band
+
+  '''
    diff = (g - r) * (g - b)
    mask1 = np.where(diff >= 0, diff, 0)
    mask2 = np.where(g > r, mask1, 0)
    green = np.where(mask2 > 0, 1, 0)
-   percent_green = np.sum(green) / float((np.shape(green)[0] * np.shape(green)[1])) * 100
-   return percent_green
+   # if pitch is 0 (horizontal), only use top half of image to calculate 
+   # % green canopy; otherwise use entire image
+   if pieces[2] == '0':
+      top_half = green[0:(0.5 * np.shape(green)[0]), :]
+      per_green = np.sum(top_half) / float((np.shape(top_half)[0] * np.shape(top_half)[1])) * 100
+   else:
+      per_green = np.sum(green) / float((np.shape(green)[0] * np.shape(green)[1])) * 100
+   return per_green
 
-#### calculate percent sky from GSV image
-#### rules are:
-#### (1) DN in blue band is >140
-#### (2) DN in blue band is greater than red DN minus 5 (blue is almost always
-####     much lower on other cover types, and in sky it is almost always > red)
-## @param r: red band
-## @param g: green band
-## @param b: blue band
-## @param pieces: split strings of filename
 def percent_sky(r, g, b, pieces):
+  ''' calculate percent sky from GSV image. the classification rules are:
+    (1) DN in blue band is >140
+    (2) DN in blue band is greater than red DN minus 5 (blue is almost always
+        much lower on other cover types, and in sky it is almost always > red)
+
+    Args:
+      r: red band
+      g: green band
+      b: blue band
+      pieces: split strings of downloaded filename
+  '''
    mask1 = np.where(b >= 140, 1, 0)
-   mask2 = np.where(b >= (r-5), 1, 0)
+   mask2 = np.where(b >= (r - 5), 1, 0)
    sky = np.where((mask1 == 1) & (mask2 == 1), 1, 0)
    # if pitch is 0 (horizontal), only use top half of image to calculate 
    # % visible sky; otherwise use entire image
    if pieces[2] == '0':
-      top_half = sky[0:(0.5*np.shape(sky)[0]), :]
+      top_half = sky[0:(0.5 * np.shape(sky)[0]), :]
       per_sky = np.sum(top_half) / float((np.shape(top_half)[0] * np.shape(top_half)[1])) * 100
    else:
       per_sky = np.sum(sky) / float((np.shape(sky)[0] * np.shape(sky)[1])) * 100
    return per_sky
 
 
-#--------------------#
-#-- define globals --#
-#--------------------#
+#-----------------------#
+#-- define parameters --#
+#-----------------------#
 base_url = "https://maps.googleapis.com/maps/api/streetview?size=1200x800&location="
 outpath = 'F:/Users/Jason/Desktop/UHI_GSV/Downloads/'
 key = "&key=AIzaSyDAmTFEHOq8xxdXMJf4rla2_SspPVpxfYs"
@@ -98,13 +113,14 @@ locations = pd.read_csv('F:/Users/Jason/Desktop/UHI_GSV/sensor_locations.csv')
 #--------------------#
 #-- acquire images --#
 #--------------------#
-get_GSV(locations = locations, 
-        outpath = outpath)
+get_GSV(locations=locations, 
+        outpath=outpath)
 
 
 #-------------------------------#
 #-- calculate percent sky/veg --#
 #-------------------------------#
+start_time = time.clock()
 ## empty dictionary for results
 results = {'SID': [],
            'heading': [],
@@ -119,47 +135,50 @@ images_list = glob.glob(path1 + '/*.jpg')
 
 ## for each image in the folder...
 for image_path in images_list:
-    
-   # read image
-   img1 = imread(image_path).astype(float)
+  try:
+    # read image
+    img1 = imread(image_path).astype(float)
 
-   # split filename into essential components (to extract pitch/heading/site name)
-   pieces = image_path.split("\\", 2)[-1]
-   pieces = pieces.split("_", 3)
-   pieces[2] = pieces[2].replace('.jpg', "")
+    # split filename into essential components (to extract pitch/heading/site name)
+    pieces = image_path.split("//", 2)[-1]
+    pieces = pieces.split("_", 3)
+    pieces[2] = pieces[2].replace('.jpg', "")
 
-   # define RGB bands
-   r = img1[:, :, 0]
-   g = img1[:, :, 1]
-   b = img1[:, :, 2]
+    # define RGB bands
+    r = img1[:, :, 0]
+    g = img1[:, :, 1]
+    b = img1[:, :, 2]
 
-   # compute green veg mask
-   per_green = percent_veg(r, g, b)
+    # compute green veg mask
+    per_green = percent_veg(r, g, b, pieces)
 
-   # compute sky mask
-   per_sky = percent_sky(r, g, b, pieces)
+    # compute sky mask
+    per_sky = percent_sky(r, g, b, pieces)
 
-   # add results to dictionary
-   results['SID'].append(pieces[0])
-   results['heading'].append(pieces[1])
-   results['pitch'].append(pieces[2])
-   results['per_green'].append(per_green)
-   results['per_sky'].append(per_sky)
+    # add results to dictionary
+    results['SID'].append(pieces[0])
+    results['heading'].append(pieces[1])
+    results['pitch'].append(pieces[2])
+    results['per_green'].append(per_green)
+    results['per_sky'].append(per_sky)
+  except:
+    pass
 
 ## convert dictionary to dataframe and write to file
 df1 = pd.DataFrame(results)
-df1.to_csv(path1 + "/_raw_results.csv", index = False)
+df1.to_csv(path1 + "/_raw_results.csv", index=False)
+print(" %s seconds" % (time.clock() - start_time))
 
 
 #-----------------------------------------------#
 #-- aggregate data to get green view/sky view --#
 #-- at each pitch                             --#
 #-----------------------------------------------#
-data = pd.read_csv('F:/Users/Jason/Desktop/UHI_GSV/Downloads/_results.csv')
+data = pd.read_csv(path1 + '/_raw_results.csv')
 
 means = data.groupby(['SID', 'pitch']).agg(['mean'])
-means.reset_index(inplace = True)
+means.reset_index(inplace=True)
 means.columns = ['SID', 'pitch', 'heading', 'per_green', 'per_sky']
 means = means.drop('heading', 1)
 
-means.to_csv(path1 + "/_means.csv", index = False)
+means.to_csv(path1 + "/_means.csv", index=False)
